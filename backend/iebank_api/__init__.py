c1c437f4287b473ca2a9d10581ca4e7b63f6d58a
vtwoptwo <vera.prohaska@gmail.com>
2022-11-06 23:50:13 +0100

    final testing

--- backend/iebank_api/__init__.py
+++ backend/iebank_api/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:srbija@localhost:5432/iebank'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:[password]@localhost:5432/iebank'
 db = SQLAlchemy(app)
 
 from iebank_api.models import Account
