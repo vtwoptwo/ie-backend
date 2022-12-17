0bb6afc2325342c4dfa3040f29857242e11424f0
vtwoptwo <84085010+vtwoptwo@users.noreply.github.com>
2022-12-17 18:55:59 +0100

    updating configsettings

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=os.getenv('DBUSER'),
     dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST') + ".postgres.database.azure.com",
+    dbhost=os.getenv('DBHOST'),
     dbname=os.getenv('DBNAME')
     )
 
