5417c16281baf145de3193bdd82c596384fed250
vtwoptwo <84085010+vtwoptwo@users.noreply.github.com>
2022-12-19 01:22:28 +0100

    updating tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,18 @@
 import pytest
 from backend_api.models import Recipe
-from backend_api import db, app
+from backend_api import db, app 
+from backend_api.routes import *
 
 
 @pytest.fixture
 def testing_client(scope='module'):
-    
-    db.create_all()
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+            db.session.add(Recipe(name='soup', ingredients='water', instructions='boil', favorite=False, rating=4))
+            db.session.commit()
+            yield client
+            yield testing_client
+            db.drop_all()
 
-    recipe = Recipe('soup', 'water', 'boil', False, 4)
-    
-    db.session.add(recipe)
-    db.session.commit()
-
-
-    with app.test_client() as testing_client:
-        yield testing_client()
-
-    db.drop_all()
-
+import pytest
--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -1,9 +1,9 @@
 from backend_api import app
-import pytest 
+import pytest
+
 
 def test_dummy_wrong_route(): 
     with app.test_client() as client:
         response = client.get('/wrong_route')
         assert response.status_code == 404
-
 
